833f13d95fe4334b7a682e646f50d5c44b52e1c7
esther olubiyi <olubiyisther@gmail.com>
2025-06-27 12:42:33 +0100

    user interface card and detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
     <Image
       src={light}
       alt="chia"
-      className="w-full  object-cover object-top rounded-md min-h-[62vh] md:h-[80vh]"
+      className="w-full  object-cover object-top rounded-md min-h-[82vh] md:h-[80vh]"
     />
 
 
@@ -75,7 +75,7 @@ export default function Home() {
             <span className="absolute inset-0 bg-white transform -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-in-out"></span>
           </button>
 
-          <button className="bg-white text-black px-6 py-2 rounded shadow opacity-70 hover:opacity-100 hover:border-[#3e80e4] hover:border-4 transition w-1/3">
+          <button className="bg-white text-black px-2  md:px-6 py-2 rounded shadow opacity-70 hover:opacity-100 hover:border-[#3e80e4] hover:border-4 transition w-1/2 md:w-1/3 whitespace-nowrap ">
             <Link href="/blog">More about Us</Link>
           </button>
         </div>
